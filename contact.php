1d0a0d91963b1f80adfe60aab11a8ed566d130ba
GarretMDeGrande <144293313+GarretMDeGrande@users.noreply.github.com>
2024-01-07 11:20:22 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+mail('thedegrandes@gmail.com', 'Test Subject', 'Test Message');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the values from the form
