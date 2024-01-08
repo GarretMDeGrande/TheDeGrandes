6c587b7874303050313411f645d8af2abd59718e
GarretMDeGrande <144293313+GarretMDeGrande@users.noreply.github.com>
2024-01-07 16:28:15 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,26 +1,26 @@
 <?php
-// Example using PHPMailer
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+// Get the form data
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-$mail = new PHPMailer(true);
-try {
-    $mail->setFrom('noreply@https://garretmdegrande.github.io/TheDeGrandes/');
-    $mail->addAddress('thedegrandes@gmail.com');
-    $mail->Subject = 'Mail From Website';
-    $mail->Body = "firstname = $firstname\r\nlastname = $lastname\r\nemail = $email\r\nmessage = $message";
-    $mail->send();
-    echo 'Mail sent successfully.<br>';
-    header("Location: thankyou.html");
-    exit();
-} catch (Exception $e) {
-    echo "Error: Message could not be sent. Mailer Error: {$mail->ErrorInfo}<br>";
-}
-if (mail($to, $subject, $txt, $headers)) {
-    echo "Mail accepted for delivery.<br>";
-} else {
-    echo "Error: Message could not be sent.<br>";
-}
+// Set up the email body
+$body = "Name: $firstname $lastname\n";
+$body .= "Phone Number: $phone\n";
+$body .= "Email: $email\n";
+$body .= "Message: $message\n";
+
+// Send the email
+$to = "thedegrandes@gmail.com";
+$subject = "Contact Form Submission";
+$headers = "From: $email";
+
+mail($to, $subject, $body, $headers);
+
+// Redirect to the thank you page
+header('Location: thankyou.html');
+
 ?>
